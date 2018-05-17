b99fdc10df387dda596f3a75b3e82134a350f7ff
Xavier Arques <xavierarques@yahoo.com>
2018-05-17 17:04:49 +0200

    Code review: Creating the renderComment method at the top of the file means that the methods it references are declared below it.
    BEST PRACTICE:
    Functions should be declared before being called. It's a convention which improves the code base's readability.

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -10,68 +10,6 @@ class CommentList extends Component {
   state = {
     editIndex: -1
   };
-  renderComment(comment, i) {
-    return (
-      <div className="comment" key={comment.id}>
-        <p className="comment-author">
-          <strong>{comment.author}</strong>
-          <br />
-          <span className="post-comment-votes-count">
-            {" "}
-            {comment.voteScore} vote{Math.abs(comment.voteScore) > 1 ? "s" : ""}
-          </span>
-        </p>
-        <p className="comment-body">{comment.body}</p>
-        <div
-          onClick={e => this.props.fetchUpVoteComment(comment.id)}
-          className="post-comment-upvote"
-        >
-          <FontAwesomeIcon icon={faThumbsUp} />
-        </div>
-        <div
-          onClick={e => this.props.fetchDownVoteComment(comment.id)}
-          className="post-comment-downvote"
-        >
-          <FontAwesomeIcon icon={faThumbsDown} />
-        </div>
-        <div
-          onClick={e => this.handleEditComment(i)}
-          className="post-comment-edit"
-        >
-          <FontAwesomeIcon icon={faEdit} />
-        </div>
-        <div
-          onClick={e => this.props.deleteComment(comment.parentId, comment.id)}
-          className="post-comment-delete"
-        >
-          <FontAwesomeIcon icon={faTrash} />
-        </div>
-      </div>
-    );
-  }
-
-  handleEditComment(i) {
-    this.setState(state => ({
-      editIndex: i
-    }));
-  }
-
-  renderEditComment(comment) {
-    if (comment) {
-      return <form key={comment.id} ref="commentForm" className="comment-form" onSubmit={e => this.handleClick(e)}>
-          <input type="text" ref="id" defaultValue={comment.id} hidden />
-
-          <input className="comment-form-author" type="text" ref="author" defaultValue={comment.author} readOnly/>
-          <input className="comment-form-body" type= "text" ref="body" defaultValue={comment.body} />
-          <input type="submit" hidden />
-        </form>;
-    }
-    return <form ref="commentForm" className="comment-form" onSubmit={e => this.handleSubmit(e)}>
-        <input className="comment-form-author" type="text" ref="author" placeholder="author" />
-        <input className="comment-form-body" type="textarea" ref="body" placeholder="Your comment" />
-        <input type="submit" hidden />
-      </form>;
-  }
 
   handleSubmit(e) {
     e.preventDefault();
@@ -84,11 +22,60 @@ class CommentList extends Component {
 
   handleClick(e) {
     e.preventDefault();
-    const commentId  = this.refs.id.value;
+    const commentId = this.refs.id.value;
     const body = this.refs.body.value;
     this.props.editComment(commentId, body);
     // Reset the state
     this.handleEditComment(-1);
+  }
+
+  handleEditComment(i) {
+    this.setState(state => ({
+      editIndex: i
+    }));
+  }
+
+  renderComment(comment, i) {
+    return <div className="comment" key={comment.id}>
+        <p className="comment-author">
+          <strong>{comment.author}</strong>
+          <br />
+          <span className="post-comment-votes-count">
+            {" "}
+            {comment.voteScore} vote{Math.abs(comment.voteScore) > 1 ? "s" : ""}
+          </span>
+        </p>
+        <p className="comment-body">{comment.body}</p>
+        <div onClick={e => this.props.fetchUpVoteComment(comment.id)} className="post-comment-upvote">
+          <FontAwesomeIcon icon={faThumbsUp} />
+        </div>
+        <div onClick={e => this.props.fetchDownVoteComment(comment.id)} className="post-comment-downvote">
+          <FontAwesomeIcon icon={faThumbsDown} />
+        </div>
+        <div onClick={e => this.handleEditComment(i)} className="post-comment-edit">
+          <FontAwesomeIcon icon={faEdit} />
+        </div>
+        <div onClick={e => this.props.deleteComment(comment.parentId, comment.id)} className="post-comment-delete">
+          <FontAwesomeIcon icon={faTrash} />
+        </div>
+      </div>;
+  }
+
+  renderEditComment(comment) {
+    if (comment) {
+      return <form key={comment.id} ref="commentForm" className="comment-form" onSubmit={e => this.handleClick(e)}>
+          <input type="text" ref="id" defaultValue={comment.id} hidden />
+
+          <input className="comment-form-author" type="text" ref="author" defaultValue={comment.author} readOnly />
+          <input className="comment-form-body" type="text" ref="body" defaultValue={comment.body} />
+          <input type="submit" hidden />
+        </form>;
+    }
+    return <form ref="commentForm" className="comment-form" onSubmit={e => this.handleSubmit(e)}>
+        <input className="comment-form-author" type="text" ref="author" placeholder="author" />
+        <input className="comment-form-body" type="textarea" ref="body" placeholder="Your comment" />
+        <input type="submit" hidden />
+      </form>;
   }
 
   render() {
