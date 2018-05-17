cba71bb595b64dd4dba6d7a537c41f3e3d3b415d
Xavier Arques <xavierarques@yahoo.com>
2018-05-17 17:42:38 +0200

    Code review: As React developers we should still put user experience 'front-and-center'. Tip: A link to the main page below the error message would improve the user's experience.

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default () => {
-  return <div className="error-message">404</div>;
-};
+const NoMatch = ({ location }) => (
+  <div>
+    <h3>
+      The page <code>{location.pathname}</code> doesn't exist
+    </h3>
+    Please return to the <Link to={"/"}>Readable Application</Link>
+  </div>
+);
+
+export default NoMatch;
